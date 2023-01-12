9798e5196cea93b5f591aa44694717480e31c4d1
Vijendra Sanodiya <101079329+VIJENDRASANODIYA@users.noreply.github.com>
2023-01-12 13:24:33 +0530

    context-api-third-project-done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import NavbarCom from './Component/Navbar/NavbarCom';
+import Home from './Component/Navbar/Home';
+import About from './Component/About';
+import Blog from './Component/Navbar/Blog';
+import Contect from './Component/Navbar/Contect';
+import More from './Component/Navbar/More';
+import { ThemeContext } from './Component/ThemeContext';
+import { useState } from 'react';
 
 function App() {
+  const [theme, setTheme] = useState("light")
+  const changeHandle = () => {
+    setTheme((theme) => (theme === "light" ? "dark" : "light"))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={theme} >
+      <div className={theme} >
+        <button onClick={changeHandle} className={theme}>changeTheme</button>
+        <BrowserRouter >
+          <NavbarCom />
+
+
+
+          <Routes>
+            <Route path='/Home' element={<Home />} />
+            <Route path='/About' element={<About />} />
+            <Route path='/Blog' element={<Blog />} />
+            <Route path='/Contect' element={<Contect />} />
+            <Route path='/More' element={<More />} />
+
+
+
+
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeContext.Provider>
+
+
   );
 }
 
---
+++ src/Component/Navbar/NavbarCom.js
@@ -0,0 +1,45 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import Home from "./Home"
+// import "bootstrap/dist/js/bootstrap.bundle.min";
+import { Link } from "react-router-dom"
+import { useContext,  useState } from "react"
+import { ThemeContext } from "../ThemeContext"
+
+
+
+function NavbarCom() {
+  const theme=useContext(ThemeContext)
+    return (
+        
+
+<div className={theme}>
+            <nav className="navbar   d-flex justify-content-around">
+                <div className="container-fluid" >
+                    <a className="navbar-brand" id={theme} style={{fontSize:"35px"}} href="#">Navbar scroll</a>
+                    <div className= "d-flex  d-flex justify-content-around list-unstyled" id={theme}>
+                    <li style={{listStyleType:"none",padding:"0px 20px"}}><Link to='/Home' style={{textDecoration:"none"}} id={theme}>Home</Link></li>
+                    <li style={{listStyleType:"none",padding:"0px 20px"}}><Link to='/About'id={theme}>About</Link></li>
+                    <li style={{listStyleType:"none",padding:"0px 20px"}}><Link to='/Blog'id={theme}>Blog</Link></li>
+                    <li style={{listStyleType:"none",padding:"0px 20px"}}><Link to='/Contect'id={theme}>Contect</Link></li>
+                    <li style={{listStyleType:"none",padding:"0px 20px"}}><Link to='/More'id={theme}>More</Link></li>
+                    </div>
+
+
+
+                    <div>
+                       
+                        <form className="d-flex justify-content-around" role="search">
+                            
+                            <button style={{padding:"0px 20px"}} className="btn btn-outline-success" type="submit">Login</button>
+                            <button  style={{padding:"0px 20px" , margin:"0px 25px"}} className="btn btn-outline-success" type="submit">Contect</button>
+                            
+                         
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </div>
+ 
+    )
+}
+export default NavbarCom 
